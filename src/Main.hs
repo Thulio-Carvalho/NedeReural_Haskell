ac2a32218ed58432b255c280aa62e246b69494e2
Thúlio Carvalho <thulioicc@gmail.com>
2018-10-26 14:57:00 -0300

    changed main to read command line arguments

--- src/Main.hs
+++ src/Main.hs
@@ -3,17 +3,19 @@
 module Main where
 import Training
 import Execution
+import System.Environment
 
 main :: IO ()
 main = do
-    option <- getLine
-    menu option
+    args <- getArgs
+    menu (args)
     return ()-- action apenas para satisfazer o retoro de IO ().
 
-menu :: String -> IO String -- a decidir o tipo da IO Action
-menu option
-    | option == "exec" = do answer <- execute
-                            putStrLn answer
-                            return ""
-    | (head $ words option) == "train" = train $ read ((words option) !! 1)
+menu :: [String] -> IO String -- a decidir o tipo da IO Action
+menu (x:xs)
+    | x == "exec" = do 
+                    answer <- execute
+                    putStrLn answer
+                    return ""
+    | x == "train" = train (read (head xs))
     | otherwise = return ("Opcao invalida!")
